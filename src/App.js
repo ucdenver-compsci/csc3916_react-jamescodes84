7520d958578fc063cb0fa4aadb6e8bfe43fede32
jamescodes84 <james.reynolds9@icloud.com>
2024-04-25 11:24:36 -0600

    test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Authentication from './components/authentication';
 import {HashRouter, Route} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './stores/store';
-
+/*test comment */
 function App() {
   return (
     <div className="App">
