6751554030875d8d8bf7e53f899dec98daae4c6b
jamescodes84 <james.reynolds9@icloud.com>
2024-04-28 16:11:50 -0600

    test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <div>
             <MovieHeader />
             <Route exact path="/" render={()=><MovieList />}/>
-            <Route exact path="/movies" render={()=><MovieList />}/>
+            <Route exact path="/movielist" render={()=><MovieList />}/>
             <Route exact path="/movie/:movieId?reviews" render={()=><Movie />}/>
             <Route path="/signin" render={()=><Authentication />}/>
           </div>
