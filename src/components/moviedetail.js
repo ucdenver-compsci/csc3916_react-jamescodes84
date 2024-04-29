a1773af7776c0b369380c7c0a09335bb339d1cec
jamescodes84 <james.reynolds9@icloud.com>
2024-04-28 20:12:27 -0600

    t

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -27,35 +27,30 @@ class MovieDetail extends Component {
             }
 
             return (
-            <Card>
-                <Card.Header>Movie Detail</Card.Header>
-                <Card.Body>
-                    <Image className="image" src={selectedMovie.imageUrl} thumbnail />
-                </Card.Body>
-
-                <ListGroup>
-                    <ListGroupItem>{selectedMovie.title}</ListGroupItem>
-                    <ListGroupItem>
-                        {selectedMovie.actors.map((actor, i) =>
+                <Card>
+                    <Card.Header>Movie Detail</Card.Header>
+                    <Card.Body>
+                        <Image className="image" src={this.props.selectedMovie.imageUrl} thumbnail />
+                    </Card.Body>
+                    <ListGroup>
+                        <ListGroupItem>{this.props.selectedMovie.title}</ListGroupItem>
+                        <ListGroupItem>
+                            {this.props.selectedMovie.actors.map((actor, i) =>
+                                <p key={i}>
+                                    <b>{actor.actorName}</b> {actor.characterName}
+                                </p>)}
+                        </ListGroupItem>
+                        <ListGroupItem><h4><BsStarFill/> {this.props.selectedMovie.avgRating}</h4></ListGroupItem>
+                    </ListGroup>
+                    <Card.Body>
+                        {this.props.selectedMovie.reviews.map((review, i) =>
                             <p key={i}>
-                                <b>{actor.actorName}</b> as {actor.characterName}
+                                <b>{review.username}</b>&nbsp; {review.review}
+                                &nbsp;  <BsStarFill /> {review.rating}
                             </p>
                         )}
-                    </ListGroupItem>
-                    <ListGroupItem><h4><BsStarFill/> {selectedMovie.avgRating.toFixed(1)}</h4></ListGroupItem>
-                </ListGroup>
-
-                <Card.Body>
-                    {selectedMovie.reviews.map((review, i) =>
-                        <p key={i}>
-                            <b>{review.username}</b> {review.review}
-                            &nbsp; <BsStarFill /> {review.rating}
-                        </p>
-                    )}
-                    
-                </Card.Body>
-                
-            </Card>
+                    </Card.Body>
+                </Card>
             
         
             
