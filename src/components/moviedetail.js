28177381bbe49ab30a6eb0e14654235daf183487
jamescodes84 <james.reynolds9@icloud.com>
2024-04-28 19:14:02 -0600

    test

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -1,4 +1,68 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchMovie } from '../actions/movieActions';
+import { Card, ListGroup, ListGroupItem, Image } from 'react-bootstrap';
+import { BsStarFill } from 'react-icons/bs';
+import './MovieDetail.css';  // Assume a CSS file for styling
+
+class MovieDetail extends Component {
+
+    componentDidMount() {
+        const { dispatch, movieId } = this.props;
+        if (!this.props.selectedMovie) {
+            dispatch(fetchMovie(movieId));
+        }
+    }
+
+    render() {
+        const { selectedMovie } = this.props;
+        if (!selectedMovie) {
+            return <div className="loading">Loading...</div>;
+        }
+
+        return (
+            <div className="movie-detail">
+                <Card>
+                    <Card.Header>{selectedMovie.title}</Card.Header>
+                    <Card.Body>
+                        <Image src={selectedMovie.imageUrl} thumbnail />
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        {selectedMovie.actors.map((actor, i) => (
+                            <ListGroupItem key={i}>
+                                <b>{actor.actorName}</b> as {actor.characterName}
+                            </ListGroupItem>
+                        ))}
+                        <ListGroupItem>
+                            <h4><BsStarFill /> {selectedMovie.avgRating.toFixed(1)}</h4>
+                        </ListGroupItem>
+                    </ListGroup>
+                    <Card.Body>
+                        {selectedMovie.reviews.map((review, i) => (
+                            <Card key={i} className="review-card">
+                                <Card.Body>
+                                    <Card.Title>{review.username} - <BsStarFill /> {review.rating}</Card.Title>
+                                    <Card.Text>{review.review}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        ))}
+                    </Card.Body>
+                </Card>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    selectedMovie: state.movie.selectedMovie
+});
+
+export default connect(mapStateToProps)(MovieDetail);
+
+
+
+
+/*import React, { Component } from 'react';
 import { fetchMovie } from "../actions/movieActions";
 import {connect} from 'react-redux';
 import {Card, ListGroup, ListGroupItem } from 'react-bootstrap';
@@ -67,3 +131,4 @@ const mapStateToProps = state => {
 
 export default connect(mapStateToProps)(MovieDetail);
 
+*/
